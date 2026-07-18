f7aa0919fa84195fb3fad195081d64111b52db33
Mars <marsjdriver+github@gmail.com>
2026-07-18 14:47:55 +0100

    changed "Customise profile" page layout

--- GUI/gui.py
+++ GUI/gui.py
@@ -19,7 +19,7 @@ def GUI_signup_login(accounts):
         [sg.Push(),
         sg.TabGroup([[
             sg.Tab("", layouts.signup_layout, visible=True, k="-SIGNUP-"),
-            sg.Tab("", layouts.customise_profile_layout, visible=True, k="-CUSTOMISE-"),
+            sg.Tab("", layouts.customise_profile_layout, visible=False, k="-CUSTOMISE-"),
             sg.Tab("", layouts.login_layout, visible=False, k="-LOGIN-")
         ]]), sg.Push()],
     ]
