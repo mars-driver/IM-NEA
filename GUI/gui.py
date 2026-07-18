b87ed0016b442ebc4b5758aa287041aa267e66d7
Mars <marsjdriver+github@gmail.com>
2026-07-18 12:08:56 +0100

    created procedure to change pages

--- GUI/gui.py
+++ GUI/gui.py
@@ -21,12 +21,14 @@ def GUI_signup_login(accounts):
         [sg.Push(),
         sg.TabGroup([[
             sg.Tab("", layouts.signup_layout, visible=True, k="-SIGNUP-"),
-            sg.Tab("", layouts.customise_profile_layout, visible=True, k="-CUSTOMISE-"),
-            sg.Tab("", layouts.login_layout, visible=True, k="-LOGIN-")
+            sg.Tab("", layouts.customise_profile_layout, visible=False, k="-CUSTOMISE-"),
+            sg.Tab("", layouts.login_layout, visible=False, k="-LOGIN-")
         ]]), sg.Push()],
     ]
 
     window = sg.Window("sign up / log in", layout, resizable=True)
+    pages = ["-SIGNUP-", "-CUSTOMISE-", "-LOGIN-"]
+    current_page = "SIGNUP"
 
     while True:
         event, values = window.read()
@@ -37,18 +39,21 @@ def GUI_signup_login(accounts):
             result = sign_up.proto_sign_up(relevant_values, accounts)
             window["SIGNUP-OUTPUT"].update(result)
             if result == "Sign up successful.":
-                window["-CUSTOMISE-"].update(visible=True)
-                window["-SIGNUP-"].update(visible=False)
+                change_page(window, pages, "-CUSTOMISE-")
         if event == "log in":
             relevant_values = [values["LOGIN-USERNAME"], values["LOGIN-PASSWORD"]]
             result = log_in.proto_log_in(relevant_values, accounts)
             window["LOGIN-OUTPUT"].update(result)
         if event == "sign up here":
-            window["-SIGNUP-"].update(visible=True)
-            window["-LOGIN-"].update(visible=False)
+            change_page(window, pages, "-SIGNUP-")
         if event == "log in here":
-            window["-LOGIN-"].update(visible=True)
-            window["-SIGNUP-"].update(visible=False)
+            change_page(window, pages, "-LOGIN-")
 
+
+def change_page(window, pages, new_page):
+    window[new_page].update(visible=True)
+    for page in pages:
+        if page != new_page:
+            window[page].update(visible=False)
 
 # sg.theme("SystemDefault1")
